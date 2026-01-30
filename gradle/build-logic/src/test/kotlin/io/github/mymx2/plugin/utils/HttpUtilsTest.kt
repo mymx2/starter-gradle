6883d7a58e42da7198975f64233c93d638d9b11f
mymx2 <32814920+mymx2@users.noreply.github.com>
2026-01-30 18:04:56 +0800

    test: HttpUtilsTest

--- gradle/build-logic/src/test/kotlin/io/github/mymx2/plugin/utils/HttpUtilsTest.kt
+++ gradle/build-logic/src/test/kotlin/io/github/mymx2/plugin/utils/HttpUtilsTest.kt
@@ -1,16 +1,17 @@
 package io.github.mymx2.plugin.utils
 
 import io.github.mymx2.fixtures.consoleLog
+import java.net.URI
 import java.time.Duration
 import org.junit.jupiter.api.Test
 
-/** HttpUtil 单元测试类 */
-class HttpUtilTest {
+/** tests for [HttpUtils] */
+class HttpUtilsTest {
 
   @Test
   fun testBaidu() {
     val baidu =
-      runCatching { HttpUtil.get("https://www.baidu.com", Duration.ofSeconds(10)) }
+      runCatching { HttpUtils.get(URI("https://www.baidu.com"), Duration.ofSeconds(10)) }
         .getOrDefault("获取失败") ?: ""
 
     consoleLog(baidu)
