9ee8041449a377a991ddd72c9e6eba83d33dd932
mymx2 <32814920+mymx2@users.noreply.github.com>
2026-07-09 00:39:20 +0800

    chore: upgrade

--- gradle/build-logic/settings.gradle.kts
+++ gradle/build-logic/settings.gradle.kts
@@ -31,7 +31,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-  val m2Version = "1.4.2"
+  val m2Version = "1.5.0"
   id("io.github.mymx2.build.feature.repositories").version(m2Version)
   id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
